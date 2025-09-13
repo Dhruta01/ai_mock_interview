d842d48d41b51aa62f747ad3959ae526275d1c64
Dhrutaclgdemo <dhruta597@gmail.com>
2025-09-13 11:31:01 +0530

    implement interview generation page ui

--- app/root/layout.tsx
+++ app/root/layout.tsx
@@ -14,10 +14,10 @@ const RootLayout = async ({children}:{children: ReactNode}) => {
   return (
     <div className='root-layout'>
       <nav>
-        <link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
          <h2 className="text-primary-100">PrepWise</h2>
-        </link>
+        </Link>
       </nav>
 
       {children}
